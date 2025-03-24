7a4e520351c5b5e63ab7d99b72cd76fac7e20267
Irrisorr <zakharkevichg@gmail.com>
2025-03-24 19:48:31 +0100

    create run method

--- src/main/java/TaskCli.java
+++ src/main/java/TaskCli.java
@@ -2,12 +2,39 @@ package main.java;
 
 import main.java.service.TaskService;
 
+import java.util.Scanner;
+
 public class TaskCli {
 
+    private static final TaskService serv = new TaskService();
+
     public static void main(String[] args) {
-        TaskService serv = new TaskService();
-        serv.addTask("test");
-        serv.addTask("test 2");
-        serv.listTasks();
+        run();
     }
+
+    private static void run(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Available commands: add, update, delete, list\nFor exit type 'exit'\n>>> ");
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            switch (command) {
+                case "add":
+                    System.out.print("Enter task description: ");
+                    String description = scanner.nextLine();
+                    serv.addTask(description);
+                    break;
+                case "list":
+                    serv.listTasks();
+                    break;
+                case "exit":
+                    scanner.close();
+                    System.exit(0);
+                    break;
+            }
+
+            System.out.print(">>> ");
+
+    }
+}
 }
