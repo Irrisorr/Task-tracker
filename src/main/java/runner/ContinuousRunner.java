688ef0e7b017165dd4e47e616ff4c578a32509ed
Irrisorr <zakharkevichg@gmail.com>
2025-04-03 17:42:50 +0200

    Update ContinuousRunner and create ArgumentsRunner

--- src/main/java/runner/ContinuousRunner.java
+++ src/main/java/runner/ContinuousRunner.java
@@ -1,17 +1,30 @@
 package main.java.runner;
 
+import main.java.mapper.JsonMapper;
+import main.java.model.TaskStatus;
 import main.java.service.TaskService;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ContinuousRunner implements Runner{
 
-    private final TaskService serv = new TaskService();
     private final Scanner scanner = new Scanner(System.in);
+    private final JsonMapper jsonMapper = new JsonMapper();
+    private final TaskService serv;
+
+    {
+        try {
+            serv = new TaskService(jsonMapper.loadTasks());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @Override
     public void run() {
-        System.out.print("Available commands: add, update $idTask, delete $idTask, list\nFor exit type 'exit'\n>>> ");
+        System.out.print(HELP_COMMAND + ">>> ");
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine().trim();
             String[] parts = line.split(" ");
@@ -22,10 +35,15 @@ public class ContinuousRunner implements Runner{
                 case "add" -> add();
                 case "update" -> update(verifyId(id));
                 case "delete" -> delete(verifyId(id));
+                case "mark-in-progress" -> markStatus(verifyId(id), TaskStatus.IN_PROGRESS);
+                case "mark-done" -> markStatus(verifyId(id), TaskStatus.DONE);
+                case "mark-todo" -> markStatus(verifyId(id), TaskStatus.TODO);
                 case "list" -> list();
                 case "exit" -> exit();
+                default -> System.out.println("Unknown command.\n" + HELP_COMMAND);
             }
 
+            synchronize();
             System.out.print(">>> ");
         }
     }
@@ -39,7 +57,6 @@ public class ContinuousRunner implements Runner{
 
     @Override
     public void update(long id) {
-        //TODO: in future rewrite verifyId method to have less garbage
         if (id == -111) {
             System.out.println("Please type valid id. Must be a number\n");
         } else {
@@ -57,12 +74,26 @@ public class ContinuousRunner implements Runner{
     }
 
     @Override
+    public void markStatus(long id, TaskStatus status) {
+        serv.markTaskStatus(id, status);
+    }
+
+    @Override
     public void list() {
         serv.listTasks();
     }
 
-    public void exit(){
+    public void exit()  {
+        synchronize();
         scanner.close();
         System.exit(0);
+    }
+
+    private void synchronize() {
+        try {
+            jsonMapper.saveTasks(serv.getTasks());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
